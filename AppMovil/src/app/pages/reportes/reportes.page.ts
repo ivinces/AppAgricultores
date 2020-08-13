623b22bd787c09a27d2b03bce3bdab227ad670a1
ivinces <ivinces@espol.edu.ec>
2020-08-13 10:09:06 -0500

    Reportes

--- AppMovil/src/app/pages/reportes/reportes.page.ts
+++ AppMovil/src/app/pages/reportes/reportes.page.ts
@@ -7,58 +7,196 @@ import * as moment from 'moment';
   templateUrl: './reportes.page.html',
   styleUrls: ['./reportes.page.scss'],
 })
+
+
 export class ReportesPage implements OnInit {
   @ViewChild('barChart') barChart;
   line: any;
   colorArray: any;
 
+  humedad:boolean;
+  radiacion:boolean;
+  temperatura:boolean;
+  year:boolean;
+  mes:boolean;
+  dia:boolean;
+  semana:boolean;
+
+  datasets: any;
+  array: {}[] = [];
+  time: {}[] = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.humedad=false;
+    this.radiacion=false;
+    this.temperatura=true;
+    this.dia=true;
   }
-
-  datasets: []
-
-  
-
 
   ionViewDidEnter() {
-    this.createLineDayChart();
+    this.createLineChart();
   }
 
-  createLineDayChart() {
+  setdataset(){
+    this.array=[];
+
+    if(this.humedad){
+      this.array.push({
+        label: 'Humedad',
+        yAxesID: 'y0',
+        data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
+        backgroundColor: '#ddee44',
+        borderColor: '#ddee44',
+        borderWidth: 1,
+        fill: false
+
+      });
+    }
+    if(this.radiacion){
+      this.array.push({
+        label: 'Radiacion',
+        data: [0.5, 0.8, 0.1, 0.9, 0.9, 0.5, 0.7, 0.12],
+        backgroundColor: '#dd1144', 
+        borderColor: '#dd1144',
+        borderWidth: 1,
+        fill: false,
+        yAxisID: 'y1'
+        
+      });
+    }
+
+    if(this.temperatura){
+      this.array.push({
+        label: 'Temperatura',
+        yAxesID: 'y0',
+        data: [17, 18, 20, 20, 19, 20, 20, 17],
+        backgroundColor: '#ddee44',
+        borderColor: '#ddee44',
+        borderWidth: 1,
+        fill: false
+
+      });
+    }
+
+  }
+
+  settimeAxesX(){
+    if(this.semana){
+      this.time=[{     
+        type: 'time',
+        time: {
+            unit: 'week'
+        }
+      }]
+    }
+    if(this.dia){
+      this.time=[{     
+        type: 'time',
+        time: {
+            unit: 'day'
+        }
+      }]
+    }
+    if(this.mes){
+      this.time=[{     
+        type: 'time',
+        time: {
+            unit: 'month'
+        }
+      }]
+    }
+    if(this.year){
+      this.time=[{     
+        type: 'time',
+        time: {
+            unit: 'year'
+        }
+      }]
+    }
+
+  }
+
+  clickDay(event){
+    this.dia=true;
+    this.semana=false;
+    this.mes=false;
+    this.year=false;
+    this.createLineChart();
+  }
+
+  clickMonth(event){
+    this.dia=false;
+    this.semana=false;
+    this.mes=true;
+    this.year=false;
+    this.createLineChart();
+  }
+
+  clickWeek(event){
+    this.dia=false;
+    this.semana=true;
+    this.mes=false;
+    this.year=false;
+    this.createLineChart();
+  }
+
+  clickYear(event){
+    this.dia=false;
+    this.semana=false;
+    this.mes=false;
+    this.year=true;
+    this.createLineChart();
+  }
+
+  clickTemp(event){
+    this.temperatura=!this.temperatura;
+    this.createLineChart();
+  }
+
+  clickRad(event){
+    this.radiacion=!this.radiacion;
+    this.createLineChart();
+  }
+
+  clickHumidity(event){
+    this.humedad=!this.humedad;
+    this.createLineChart();
+  }
+
+  clearcanvas(){
+    this.barChart.width=this.barChart.width;
+  }
+
+  createLineChart(){
+    this.setdataset();
+    this.settimeAxesX();
+    this.clearcanvas();
+
     this.line = new Chart(this.barChart.nativeElement, {
       type: 'line',
       data: {
         labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
-        datasets: [{
-          label: 'Humedad',
-          yAxesID: 'y0',
-          data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
-          backgroundColor: '#ddee44',
-          borderColor: '#ddee44',
-          borderWidth: 1,
-          fill: false
-
-        },
-        {
-          label: 'Radiacion',
-          data: [0.5, 0.8, 0.1, 0.9, 0.9, 0.5, 0.7, 0.12],
-          backgroundColor: '#dd1144', 
-          borderColor: '#dd1144',
-          borderWidth: 1,
-          fill: false,
-          yAxisID: 'y1'
-          
-        }]
+        datasets: this.array,
       },
       options: {
         scales: {
+          xAxes: [/*{
+            type: 'time',
+            time: {
+                unit: 'week'
+            }
+          }*/],
           yAxes: [
             {
               id: 'y',
               type: 'linear',
               position: 'left',
+              scaleLabel: {
+                display: true,
+                labelString: 'Temperatura/Humedad'
+              }
             }, {
               id: 'y1',
               type: 'linear',
@@ -67,112 +205,16 @@ export class ReportesPage implements OnInit {
                 max: 1,
                 min: 0
               },
+              scaleLabel: {
+                display: true,
+                labelString: 'Radiación'
+              },
               // grid line settings
 							gridLines: {
                 drawOnChartArea: false, // only want the grid lines for one axis to show up
               }
             }
           ]
-        }
-      }
-    });
-  
-  }
-  clickDia(event) {
-    this.createLineDayChart();
-  }
-
-  clickSemana(event) {
-    this.line = new Chart(this.barChart.nativeElement, {
-      type: 'line',
-      data: {
-        //labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
-        datasets: [{
-          label: 'Valores en la Semana',
-          data: this.datasets,
-          //backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
-          backgroundColor: 'rgb(0,0,0,0)',
-          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          xAxes: [{
-            type: 'time',
-            time: {
-                unit: 'week'
-            }
-          }],
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
-  }
-
-  clickMes(event) {
-    this.line = new Chart(this.barChart.nativeElement, {
-      type: 'line',
-      data: {
-        //labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
-        datasets: [{
-          label: 'Valores del Mes',
-          data: this.datasets,
-          //backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
-          backgroundColor: 'rgb(0,0,0,0)',
-          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          xAxes: [{
-            type: 'time',
-            time: {
-                unit: 'month'
-            }
-          }],
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
-  }
-
-  clickYear(event) {
-    this.line = new Chart(this.barChart.nativeElement, {
-      type: 'line',
-      data: {
-        //labels: this.arrayOfDate,
-        datasets: [{
-          label: 'Valores del Año',
-          data: this.datasets,
-          //backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
-          backgroundColor: 'rgb(0,0,0,0)',
-          borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          xAxes: [{
-            type: 'time',
-            time: {
-                unit: 'year'
-            }
-          }],
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
         }
       }
     });
