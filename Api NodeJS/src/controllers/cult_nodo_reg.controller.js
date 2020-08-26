a1f26f54727b599780836a192c9333ef3b1d782f
ivinces <ivinces@espol.edu.ec>
2020-08-26 18:05:47 -0500

    cambios

--- Api NodeJS/src/controllers/cult_nodo_reg.controller.js
+++ Api NodeJS/src/controllers/cult_nodo_reg.controller.js
@@ -11,14 +11,14 @@ const pool = new Pool({
 
 //CULTIVO NODO MATCH
 const getCultivoxNodoxRegistros = async (req, res) => {
-    const fecha="to_char( Registros.fecha_hora, 'DD-MM-YYYY HH:mm:ss') as fecha_hora"
+    const fecha="to_char( Registros.fecha_hora, 'YYYY-MM-DD HH:mm:ss') as fecha_hora"
     const response = await pool.query('SELECT Registros.id_registro,'+fecha+', Registros.temperatura, Registros.humedad, Registros.radiacion, Registros.id_nodo, latitud, longitud, Nodo.activo AS n_activo, Cultivo.activo AS c_activo, cod_nodo, nombre, descripcion, nodo_central, Nodo.id_cultivo FROM Registros, Nodo, Cultivo WHERE Cultivo.id_cultivo = Nodo.id_cultivo AND Registros.id_nodo = Nodo.id_nodo ORDER BY fecha_hora DESC');
     res.status(200).json(response.rows);
 };
 
 const getCultivoxNodoxRegistrosById = async (req, res) => {
     const id = parseInt(req.params.id);
-    const fecha="to_char( Registros.fecha_hora, 'DD-MM-YYYY HH:mm:ss') as fecha_hora"
+    const fecha="to_char( Registros.fecha_hora, 'YYYY-MM-DD HH:mm:ss') as fecha_hora"
     const response = await pool.query('SELECT Registros.id_registro,'+fecha+', Registros.temperatura, Registros.humedad, Registros.radiacion, Registros.id_nodo, latitud, longitud, Nodo.activo AS n_activo, Cultivo.activo AS c_activo, cod_nodo, nombre, descripcion, nodo_central, Nodo.id_cultivo FROM Registros, Nodo, Cultivo WHERE Cultivo.id_cultivo = Nodo.id_cultivo AND Registros.id_nodo = Nodo.id_nodo AND Cultivo.id_cultivo = $1 ORDER BY fecha_hora DESC', [id]);
     res.json(response.rows);
 };
